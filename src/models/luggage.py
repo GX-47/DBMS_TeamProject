72646d77830d5c6c6053e6784dc04f6f007142d7
Akash Anand <akashanandscorpio@gmail.com>
2024-11-19 15:01:17 +0530

    Fixed luggage

--- src/models/luggage.py
+++ src/models/luggage.py
@@ -1,17 +1,18 @@
 import streamlit as st
 from mysql.connector import Error
 from config.database import connect_to_database
+import uuid
 
 def register_luggage(booking_id, weight, category, handling_instructions, user_id):
     try:
         db = connect_to_database()
         cursor = db.cursor()
+        luggage_id = str(uuid.uuid4())[:10]  # Generate a unique ID
         query = """
-        INSERT INTO luggage (booking_id, weight, category, handling_instructions, registered_by)
-        VALUES (%s, %s, %s, %s, %s)
+        INSERT INTO luggage (luggage_id, booking_id, weight, category, handling_instructions, staff_id)
+        VALUES (%s, %s, %s, %s, %s, %s)
         """
-        cursor.execute(query, (booking_id, weight, category, handling_instructions, user_id))
-        luggage_id = cursor.lastrowid
+        cursor.execute(query, (luggage_id, booking_id, weight, category, handling_instructions, user_id))
         db.commit()
         return luggage_id
     except Error as e:
