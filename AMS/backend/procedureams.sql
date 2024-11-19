686d4f9d5e7297b6a1d04ff33f4ea533b681ee84
Akash Anand <akashanandscorpio@gmail.com>
2024-11-19 09:45:28 +0530

    Remove unused SQL procedures, functions, triggers, and frontend HTML files

--- AMS/backend/procedureams.sql
+++
@@ -1,6 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE update_food_stock(IN item_name VARCHAR(50), IN new_stock INT)
-BEGIN
-    UPDATE Food SET Stock = new_stock WHERE Item_name = item_name;
-END //
-DELIMITER ;
